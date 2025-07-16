d4382b2c0396fbe1142f41bedebb77ad392879b2
Oleksii Drenov <leshadrenev@gmail.com>
2025-07-16 23:40:16 +0300

    register models in admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tasks.models import Task, TaskType, Position, Worker
+
+admin.site.register(TaskType)
+admin.site.register(Position)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "priority", "is_completed", "task_type")
+    search_fields = ("name",)
+    list_filter = ("is_completed", "task_type", "deadline")
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    ordering = ("last_name",)
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "position",
+    )
+    list_filter = ("is_staff", "is_superuser", "is_active", "position")
+    search_fields = (
+        "username",
+        "first_name",
+        "last_name",
+    )
