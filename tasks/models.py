118e75ec1974ff8068f3559e61d36463dbaf3bc6
Oleksii Drenov <leshadrenev@gmail.com>
2025-07-16 22:56:30 +0300

    add Task, TaskType, Position, Worker models and set custom user model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import User, AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    position = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.position
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position, on_delete=models.CASCADE, null=True, blank=True
+    )
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+
+    class Priority(models.TextChoices):
+        LOW = "low", "Low"
+        MEDIUM = "medium", "Medium"
+        HIGH = "high", "High"
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=10, choices=Priority.choices, default=Priority.MEDIUM
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
+
+    class Meta:
+        ordering = ["deadline"]
+
+    def __str__(self):
+        return f"{self.name}: Deadline: {self.deadline}, Priority: {self.priority}"
