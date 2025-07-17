b4a910c6efb4571852587972c29929f23b0d5000
Oleksii Drenov <leshadrenev@gmail.com>
2025-07-17 23:43:17 +0300

    connect tasks.urls and create base/index/sidebar templates

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from tasks.models import Worker, Task
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_workers = Worker.objects.count()
+    num_tasks = Task.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_workers": num_workers,
+        "num_tasks": num_tasks,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "tasks/index.html", context=context)
