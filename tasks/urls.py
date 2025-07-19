038f6872c3003d2a29eb403b9d9be6318660d6fe
Oleksii Drenov <leshadrenev@gmail.com>
2025-07-19 23:36:06 +0300

    update styles and adjust routes for new task layout

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from tasks.views import index
+from tasks.views import index, TasksListView
 
 app_name = "tasks"
 urlpatterns = [
     path("", index, name="index"),
+path(
+        "tasks/",
+        TasksListView.as_view(),
+        name="tasks-list",
+    ),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
 from tasks.models import Worker, Task
 
@@ -19,3 +21,27 @@ def index(request):
     }
 
     return render(request, "tasks/index.html", context=context)
+
+
+class TasksListView(generic.ListView):
+    model = Task
+    context_object_name = "task_list"
+    template_name = "tasks/task_list.html"
+    paginate_by = 5
+    queryset = Task.objects.all()
+
+    def get_context_data(self, *, object_list=..., **kwargs):
+        context = super(TasksListView, self).get_context_data(**kwargs)
+        context["todo_tasks"] = Task.objects.filter(status="todo")
+        context["in_progress_tasks"] = Task.objects.filter(status="in_progress")
+        context["done_tasks"] = Task.objects.filter(status="done")
+        context["needs_review_tasks"] = Task.objects.filter(status="needs_review")
+    #     name = self.request.GET.get("name", "")
+    #     context["search_form"] = TaskSearchForm(initial={"name": name})
+        return context
+
+    # def get_queryset(self):
+    #     name = self.request.GET.get("name", "")
+    #     if name:
+    #         return self.queryset.filter(name__icontains=name)
+    #     return self.queryset
